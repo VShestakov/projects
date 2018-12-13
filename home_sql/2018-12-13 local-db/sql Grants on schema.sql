fda23eaf09c86ce0ba4e4d93bb52d4b0b6265b07
VShestakov <vlad-shestakov@mail.ru>
2018-12-14 00:49:39 +0500

    user grants

--- home_sql/2018-12-13 local-db/sql Grants on schema.sql
+++ home_sql/2018-12-13 local-db/sql Grants on schema.sql
@@ -2,44 +2,27 @@
 ---------------------------------------------------------------
 ----        grants on schema
 ---------------------------------------------------------------
--- Применимость БД: ???
---
--- ПАРАМЕТРЫ ЗАПУСКА:
---   v_dummy         - Код документа. Пример: 12323
---   RETURNS         - XML в CLOB
 
---
 -- ИСТОРИЯ ВЕРСИЙ:
 -- 14.12.2018 USER - Первая версия 
 
-GRANT select, insert, delete ON scott.emp TO VSHESTAKOV;
-grant all on scott.emp to vshestakov;
-grant all on SCOTT.DEPT TO VSHESTAKOV;
+
+-- -- Grant/Revoke object privileges 
+GRANT all ON scott.emp TO VSHESTAKOV;
+grant select, insert, update, delete, references, alter, index, debug on SCOTT.EMP to vshestakov;
 revoke all on scott.DEPT from vshestakov;
-select any table to vshestakov;
 
-select o.owner || '.' || o.table_name as tname
-      ,o.owner, o.table_name --< для отладки
-  from ALL_TABLES o
- where o.owner = 'SCOTT'
-;/**/ 
+--select any table to vshestakov;
 
-declare 
-  v_cnt  number; 
-  v_user varchar2(255) := 'VSHESTAKOV'; 
-  v_sql  varchar2(4000);
+-- grant privelege
 begin
-
   -- Выдача прав на все таблицы схемы пользователю
-  for rec in (-- Все таблицы схемы
-            select o.owner || '.' || o.table_name as tname
-                  ,o.owner, o.table_name --< для отладки
-              from ALL_TABLES o
-             where o.owner = 'SCOTT'
-            )
-  loop
-    v_sql := 'grant all on :1 TO :2;';
-    execute immediate v_sql using rec.tname, v_user;
-    dbms_output.put_line(replace(replace(v_sql, ':1', rec.tname), ':2', v_user)); --< Для отладки 
+  for rec in (
+    select --'grant all on ' || o.owner || '.' || o.table_name || ' TO VSHESTAKOV' as s
+           'revoke all on ' || o.owner || '.' || o.table_name || ' from VSHESTAKOV' as s
+      from ALL_TABLES o where o.owner = 'SCOTT'
+  ) loop
+    execute immediate rec.s;
+    dbms_output.put_line(rec.s); --< Для отладки 
   end loop;  
 end; 
