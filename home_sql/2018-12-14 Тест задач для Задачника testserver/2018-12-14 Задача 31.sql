970e5bb89600c8f39d6edd131898ee8e24809854
VShestakov <vlad-shestakov@mail.ru>
2018-12-14 01:06:18 +0500

    initial commit

---
+++ home_sql/2018-12-14 Тест задач для Задачника testserver/2018-12-14 Задача 31.sql
@@ -0,0 +1,43 @@
+-- USER 14.12.2018: 
+---------------------------------------------------------------
+----        Тест задачи 31
+---------------------------------------------------------------
+-- Применимость БД: ???
+--
+-- ПАРАМЕТРЫ ЗАПУСКА:
+--   v_dummy         - Код документа. Пример: 12323
+--   RETURNS         - XML в CLOB
+
+--
+-- ИСТОРИЯ ВЕРСИЙ:
+-- 14.12.2018 USER - Первая версия 
+-- https://testserver.pro/run/test/Oracle-PL-SQL/
+-- Вопрос 31
+
+
+select * from v$version;
+/* 
+Oracle Database 11g Enterprise Edition Release 11.2.0.1.0 - 64bit Production
+PL/SQL Release 11.2.0.1.0 - Production
+ /**/
+ 
+drop sequence aaa_seq;
+
+create sequence aaa_seq
+start with 1000
+increment by 5
+cache 5;
+
+select aaa_seq.nextval from dual; 
+select aaa_seq.currval from dual; 
+select aaa_seq.nextval from dual; 
+select aaa_seq.nextval + 25 from dual; 
+
+
+select aaa_seq.nextval from dual; 
+
+/*  
+NEXTVAL
+1015
+
+/**/
